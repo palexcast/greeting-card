0b06e76b66334d69da0786a14c5046f4b4b235df
alexander <patricio.castillo@evry.com>
2020-12-07 17:31:18 +0100

    styling and text

--- components/CardInsideRight/index.tsx
+++ components/CardInsideRight/index.tsx
@@ -13,12 +13,15 @@ export default function CardInsideRight(): JSX.Element {
                 </div>
                 <div className={styles.textContainer}>
                     <div className={styles.textTopLeft}>
-                        <span className={styles.text}>Thursday, 17th of Desember</span>
+                        <span className={styles.text}>Thursday, 17th of December</span>
                         <span className={styles.text}>19:00 - 22:00+</span>
                     </div>
-                    <span className={styles.text}>We would like to invite you to an evening filled with fun and laughter</span>
-                    <span className={styles.text}></span>
-                    <span className={styles.text}></span>
+                    <div className={styles.textMiddle}>
+                        <span className={styles.text}>We would like to invite you to an evening filled with fun and laughter</span>
+                        <span className={styles.text}>Wednesday 16th of December your leader will deliver a pleasant surprise to you.</span>
+                        <span className={styles.text + ' ' + styles.smallText}>If you're not home this evening, please let your leader know as soon as possible!</span>
+                        <span className={styles.text}>Our wish is that you enjoy this surprise alongside your colleagues, the 17th.</span>
+                    </div>
                 </div>
             </div>
         </>
