a00d943e71b9526acd2a239cb6d1458439b3b679
alexander <patricio.castillo@evry.com>
2020-12-08 20:41:18 +0100

    more styling and updated text

--- components/Countdown/index.tsx
+++ components/Countdown/index.tsx
@@ -4,12 +4,19 @@ import ReactCountdown from 'react-countdown';
 
 
 function Completed(): JSX.Element {
+    const christmasDate = new Date(2020, 11, 23, 12, 0, 0);
+
     return (
         <>
-            <span>The event has started!</span>
-        </>
-    )
-}
+            <ReactCountdown date={christmasDate} intervalDelay={1000} renderer={({completed}) => {
+                return completed ?
+                    <span className={styles.countdownComplete}>Currently enjoying christmas with our families and dear ones 🎄<br/> Merry Christmas! ⭐️</span> :
+                    <span className={styles.countdownComplete}>Currently having a good time with our dear colleagues 🎄</span>;
+            }}
+                />
+                </>
+                )
+            }
 
 function StillCounting({days, hours, minutes, seconds}): JSX.Element {
     return (
@@ -18,7 +25,8 @@ function StillCounting({days, hours, minutes, seconds}): JSX.Element {
                 {days > 0 && <span className={styles.countdownNumber}>{days}d</span>}
                 {(days > 0 || hours > 0) && <span className={styles.countdownNumber}>{hours}h</span>}
                 {(days > 0 || hours > 0 || minutes > 0) && <span className={styles.countdownNumber}>{minutes}m</span>}
-                {(days > 0 || hours > 0 || minutes > 0 || seconds > 0) && <span className={styles.countdownNumber}>{seconds}s</span>}
+                {(days > 0 || hours > 0 || minutes > 0 || seconds > 0) &&
+                <span className={styles.countdownNumber}>{seconds}s</span>}
             </span>
         </>
     )
@@ -29,7 +37,7 @@ export default function Countdown(): JSX.Element {
 
     return (
         <>
-            <ReactCountdown date={date} intervalDelay={1000}  renderer={({days, hours, minutes, seconds, completed}) => {
+            <ReactCountdown date={date} intervalDelay={1000} renderer={({days, hours, minutes, seconds, completed}) => {
                 return completed ? Completed() : StillCounting({days, hours, minutes, seconds});
             }}
             />
