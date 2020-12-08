c0782749ce1826d355488a9e2ebadeb6ec05fd64
Sindre Bøyum <sindre.boyum@gmail.com>
2020-12-08 20:52:58 +0100

    Fix typos

--- components/CardInsideRight/index.tsx
+++ components/CardInsideRight/index.tsx
@@ -17,10 +17,10 @@ export default function CardInsideRight(): JSX.Element {
                         <span className={styles.text}>19:00 - 22:00+</span>
                     </div>
                     <div className={styles.textMiddle}>
-                        <span className={styles.text + ' ' + styles.larger}>We would like to invite you to this years digital Christmas Party.</span>
+                        <span className={styles.text + ' ' + styles.larger}>We would like to invite you to this year's digital Christmas Party.</span>
                         <span
                             className={styles.text + ' ' + styles.larger}>An evening filled with fun and laughter!</span>
-                        <span className={styles.text}>Dresscode: Dress to impress!</span>
+                        <span className={styles.text}>Dress code: Dress to impress!</span>
                         <span className={styles.text}>Please let us know if you are able to attend or not within Friday 11th of December by accepting the calendar invite.</span>
                         <span
                             className={styles.text}>The program and where to meet will soon be released here. Stay in touch!</span>
@@ -28,7 +28,7 @@ export default function CardInsideRight(): JSX.Element {
                         <span className={styles.importantText}>
                             <span className={styles.text}>PS: Wednesday 16th of December your leader will deliver a pleasant surprise to you.</span>
                             <span className={styles.text}>If you’re not home this evening, please let your leader know as soon as possible!</span>
-                            <span className={styles.text}>Our wish is that you enjoy this surprise alongside your colleagues, the 17th. </span>
+                            <span className={styles.text}>Our wish is that you enjoy this surprise alongside your colleagues on the 17th. </span>
                         </span>
                     </div>
                 </div>
